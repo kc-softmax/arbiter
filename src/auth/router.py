252eac961bfb70b7961f05f8f0c864f3315adca4
jaehong0721 <newbie302@gmail.com>
2023-06-22 11:10:49 +0900

    [ADD] jwt 토큰으로 인증하기 추가

---
+++ src/auth/router.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from .dependencies import get_current_user
+from .schemas import Token, CreateUserResponse, CreateUserRequest, User
+from .service import create_user, get_user, authenticate_user
+from .utils import create_access_token, create_refresh_token
+
+router = APIRouter()
+
+
+@router.post("/signup", response_model=CreateUserResponse)
+async def signup(data: CreateUserRequest):
+    # TODO service의 구현에 따라 달라질 수 있음
+    user = get_user(data.email)
+
+    if user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+
+    # TODO service의 구현에 따라 달라질 수 있음
+    user = create_user(data.email, data.password)
+    return {
+        'id': user.id,
+    }
+
+
+@router.post('/login', response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    # TODO service의 구현에 따라 달라질 수 있음
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if user is False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+
+    return {
+        "access_token": create_access_token(user.email),
+        "refresh_token": create_refresh_token(user.email),
+    }
+
+
+# protected test(auth 인증되어야 접근 가능한 api)
+@router.get('/users/me', response_model=CreateUserResponse)
+async def get_me(user: User = Depends(get_current_user)):
+    return user
