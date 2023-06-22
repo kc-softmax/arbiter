9f3756dede819c5d359870c85810a6d8e0ba8546
윤범진 <>
2023-06-22 20:12:16 +0900

    [ADD]함수 추가, click custom param

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,9 +1,31 @@
+from typing import Any
+import click
 import typer
 from sqlmodel import Session, select
 from database import create_db_and_tables, engine
 from auth.models import User
 
 app = typer.Typer()
+
+
+# click custom type 가능하게
+_get_click_type = typer.main.get_click_type
+
+
+def supersede_get_click_type(
+    *, annotation: Any, parameter_info: typer.main.ParameterInfo
+) -> click.ParamType:
+    if hasattr(annotation, "parse_raw"):
+
+        class CustomParamType(click.ParamType):
+            def convert(self, value, param, ctx):
+                return annotation.parse_raw(value)
+
+        return CustomParamType()
+    else:
+        return _get_click_type(annotation=annotation, parameter_info=parameter_info)
+
+typer.main.get_click_type = supersede_get_click_type
 
 @app.command()
 def register_user_by_device_id(device_id: str) -> User:
@@ -48,6 +70,17 @@ def login_by_email(email: str, password: str) -> User | None:
         return user
 
 @app.command()
+def check_user_by_email(email: str) -> User | None:
+    with Session(engine) as session:
+        # first or None
+        statement = select(User).where(User.email == email)
+        results = session.exec(statement)
+        user = results.first()
+        print(user)
+        return user
+
+
+@app.command()
 def create_auth_token(user_id: int):
     return "this is auth token"
 
@@ -72,8 +105,26 @@ def delete_user(user_id: int) -> bool:
         return is_success
 
 @app.command()
-def get_user(user_id: int):
-    return "this is get user"
+def get_user(user_id: int) -> User | None:
+    with Session(engine) as session:
+        user = session.get(User, user_id)
+        print(user)
+        return user
+
+@app.command()
+def update_user(user_id: int, user: User) -> User | None:
+    with Session(engine) as session:
+        db_user = session.get(User, user_id)
+        if not db_user:
+            return None
+        hero_data = user.dict(exclude_unset=True)
+        for key, value in hero_data.items():
+            setattr(db_user, key, value)
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user
+
 
 if __name__ == "__main__":
     create_db_and_tables()
