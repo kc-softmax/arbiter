ea4757dbc399e5d0fcfbb375c57c6f3950c6b2f0
윤범진 <>
2023-06-22 16:35:10 +0900

    [ADD]login type

--- src/auth/models.py
+++ src/auth/models.py
@@ -2,6 +2,15 @@ from datetime import datetime
 from typing import Optional
 from database import engine
 from sqlmodel import Column, Field, SQLModel, String
+from enum import StrEnum
+
+class LoginType(StrEnum):
+    GUEST = "guest"
+    EMAIL = "email"
+    FACEBOOK = "facebook"
+    APPLE = "apple"
+    STEAM = "steam"
+    GOOGLE = "google"
 
 
 class User(SQLModel, table=True):
@@ -13,5 +22,6 @@ class User(SQLModel, table=True):
     display_name: Optional[str] = Field(sa_column=Column(String(128)))
     device_id: Optional[str] = Field(sa_column=Column(String(128)))
     verified: bool = False
+    login_type: Optional[LoginType] = LoginType.GUEST
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     updated_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
