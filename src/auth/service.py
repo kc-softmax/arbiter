9bf8d8a5087ab9cedf3a902bb36afd3eada77417
윤범진 <>
2023-06-22 10:41:52 +0900

    [ADD]간단한 로그인 로직

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,36 +1,79 @@
 import typer
+from sqlmodel import Session, select
+from auth.models import User, create_db_and_tables, engine
 
 app = typer.Typer()
 
-# def registerUserByEmail
-
-# def loginByEmail
-
-# def registerUserByDeviceId? (guest)
-
-# def loginByDeviceId? (guest)
-
-# def deleteUser
-
-# def getUser
-
-# def createAuthToken
-
-# def refreshAuthToken
-
+@app.command()
+def register_user_by_device_id(device_id: str) -> User:
+    with Session(engine) as session:
+        user = User(device_id=device_id)
+        session.add(user)
+        session.commit()
+        #table 값을 객체에 부여해준다.
+        session.refresh(user)
+        print(user)
+        return user
 
 @app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-
+def login_by_device_id(device_id: str) -> User:
+    with Session(engine) as session:
+        # first or None
+        statement = select(User).where(User.device_id == device_id)
+        results = session.exec(statement)
+        user = results.first()
+        print(user)
+        return user
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def resister_user_by_email(email: str, password: str) -> User:
+    with Session(engine) as session:
+        user = User(email=email, password=password)
+        session.add(user)
+        session.commit()
+        #table 값을 객체에 부여해준다.
+        session.refresh(user)
+        print(user)
+        return user
 
+@app.command()
+def login_by_email(email: str, password: str) -> User | None:
+    with Session(engine) as session:
+        # first or None
+        statement = select(User).where(User.email == email).where(User.password == password)
+        results = session.exec(statement)
+        user = results.first()
+        print(user)
+        return user
+
+@app.command()
+def create_auth_token(user_id: int):
+    return "this is auth token"
+
+@app.command()
+def refresh_auth_token(auth_token: str):
+    return "this is refresh token"
+
+@app.command()
+def delete_user(user_id: int) -> bool:
+    is_success = False
+    with Session(engine) as session:
+        statement = select(User).where(User.id == user_id)
+        results = session.exec(statement)
+        try:
+            # no row exception 처리
+            user = results.one()
+            session.delete(user)
+            session.commit()
+            is_success = True
+        except Exception as e:
+            print(e)
+        return is_success
+
+@app.command()
+def get_user(user_id: int):
+    return "this is get user"
 
 if __name__ == "__main__":
+    create_db_and_tables()
     app()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+from auth.service import login_by_device_id, register_user_by_device_id
+from src.auth.models import create_db_and_tables
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 def read_root():
@@ -13,3 +18,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/register/{device_id}")
+def login(device_id: str):
+    return register_user_by_device_id(device_id=device_id)
+
+
+@app.get("/login/{device_id}")
+def login(device_id: str):
+    return login_by_device_id(device_id=device_id)
