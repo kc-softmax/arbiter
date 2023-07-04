8b537d0ece554e61eab88bb9440eca5646e01252
jaehong0721 <newbie302@gmail.com>
2023-07-04 14:19:33 +0900

    [FIX] open api schema 생성 에러 수정

--- src/main.py
+++ src/main.py
@@ -6,9 +6,33 @@ from fastapi.responses import JSONResponse
 
 from .auth.router import router as auth_router, login
 from .exceptions import BadRequest
-from .utils import update_openapi_schema_name, create_custom_openapi
+from .utils import update_openapi_schema_name
 
 app = FastAPI()
+
+
+def create_custom_openapi():
+    if not app.openapi_schema:
+        app.openapi_schema = get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            terms_of_service=app.terms_of_service,
+            contact=app.contact,
+            license_info=app.license_info,
+            routes=app.routes,
+            tags=app.openapi_tags,
+            servers=app.servers,
+        )
+        for _, method_item in app.openapi_schema.get('paths').items():
+            for _, param in method_item.items():
+                responses = param.get('responses')
+                # remove 422 response, also can remove other status code
+                if '422' in responses:
+                    del responses['422']
+
+    return app.openapi_schema
 
 
 @app.exception_handler(RequestValidationError)
--- src/utils.py
+++ src/utils.py
@@ -11,30 +11,6 @@ def update_openapi_schema_name(app: FastAPI, function: Callable, name: str) -> N
             return
 
 
-def create_custom_openapi():
-    if not app.openapi_schema:
-        app.openapi_schema = get_openapi(
-            title=app.title,
-            version=app.version,
-            openapi_version=app.openapi_version,
-            description=app.description,
-            terms_of_service=app.terms_of_service,
-            contact=app.contact,
-            license_info=app.license_info,
-            routes=app.routes,
-            tags=app.openapi_tags,
-            servers=app.servers,
-        )
-        for _, method_item in app.openapi_schema.get('paths').items():
-            for _, param in method_item.items():
-                responses = param.get('responses')
-                # remove 422 response, also can remove other status code
-                if '422' in responses:
-                    del responses['422']
-
-    return app.openapi_schema
-
-
 def exceptions_to_openapi_response(code: int, detail: str) -> dict:
     response = {}
     response[code] = {
