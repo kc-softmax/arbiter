c17281e62cdcd46c28e1c23249acd2b2eb82dd70
윤범진 <unknown>
2023-07-06 15:16:46 +0900

    [TEST]test 함수 추가

--- src/auth/cli.py
+++ src/auth/cli.py
@@ -3,7 +3,8 @@ import sys
 import uuid
 import typer
 from rich import print
-from auth import service
+
+from auth.service import UserService, ConsoleUserService
 from auth.models import User
 from database import create_db_and_tables
 
@@ -13,7 +14,7 @@ app = typer.Typer()
 
 @app.command('register_user_by_device_id')
 def register_user_by_device_id(device_id: str = typer.Argument(uuid.uuid4())):
-    if user := service.register_user_by_device_id(
+    if user := UserService().register_user_by_device_id(
         device_id=device_id
     ):
         print("[green]{0} Success! Device ID: {1}".format(get_current_function(), device_id))
@@ -23,7 +24,7 @@ def register_user_by_device_id(device_id: str = typer.Argument(uuid.uuid4())):
 
 @app.command('login_by_device_id')
 def login_by_device_id(device_id: str = typer.Argument('')):
-    if user := service.login_by_device_id(device_id=device_id):
+    if user := UserService().login_by_device_id(device_id=device_id):
         print("[green]{0} Success! Device ID: {1}".format(get_current_function(), device_id))
     else:
         print("[red]{0} Failed!".format(get_current_function()))
@@ -34,7 +35,7 @@ def resister_user_by_email(
     email: str = typer.Argument('test@test.com'),
     password: str = typer.Argument('password')
 ):
-    if user := service.resister_user_by_email(
+    if user := UserService().resister_user_by_email(
         email=email,
         password=password
     ):
@@ -48,7 +49,7 @@ def login_by_email(
     email: str = typer.Argument('test@test.com'),
     password: str = typer.Argument('password')
 ):
-    if user := service.login_by_email(
+    if user := UserService().login_by_email(
         email=email,
         password=password
     ):
@@ -59,7 +60,7 @@ def login_by_email(
 
 @app.command('check_user_by_email')
 def check_user_by_email(email: str = typer.Argument('test@test.com')):
-    if user := service.check_user_by_email(
+    if user := UserService().check_user_by_email(
         email=email
     ):
         print("[green]{0} Success! Email: {1}".format(get_current_function(), email))
@@ -69,7 +70,7 @@ def check_user_by_email(email: str = typer.Argument('test@test.com')):
 
 @app.command('delete_user')
 def delete_user(user_id: int = typer.Argument(1)):
-    if is_success := service.delete_user(user_id=user_id):
+    if is_success := UserService().delete_user(user_id=user_id):
         print("[green]{0} Success! User ID: {1}".format(get_current_function(), user_id))
     else:
         print("[red]{0} Failed!".format(get_current_function()))
@@ -77,7 +78,7 @@ def delete_user(user_id: int = typer.Argument(1)):
 
 @app.command('get_user')
 def get_user(user_id: int = typer.Argument(1)):
-    if user := service.get_user(user_id=user_id):
+    if user := UserService().get_user(user_id=user_id):
         print("[green]{0} Success! User ID: {1}".format(get_current_function(), dict(user)))
     else:
         print("[red]{0} Failed!".format(get_current_function()))
@@ -88,11 +89,75 @@ def update_user(
     user_id: int = typer.Argument(1),
     user: str = typer.Argument('{"display_name": "test1234", "verified": true}')
 ):
-    if user := service.update_user(
+    if user := UserService().update_user(
         user_id=user_id,
         user=User(**json.loads(user))
     ):
         print("[green]{0} Success! User: {1}".format(get_current_function(), dict(user)))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('register_console_user')
+def register_console_user(
+    email: str = typer.Argument('tesst@test.com'),
+    password: str = typer.Argument('password'),
+    user_name: str = typer.Argument('test_user'),
+    role: str = typer.Argument('owner')
+):
+    if console_user := ConsoleUserService().register_console_user(
+        email=email,
+        password=password,
+        user_name=user_name,
+        role=role
+    ):
+        print("[green]{0} Success! User: {1}".format(get_current_function(), dict(console_user)))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('get_console_user_all')
+def get_console_user_all():
+    if console_users := ConsoleUserService().get_console_user_all():
+        print("[green]{0} Success! User: {1}".format(get_current_function(), console_users))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('get_console_by_role')
+def get_console_by_role(role: str = typer.Argument('owner')):
+    if console_users := ConsoleUserService().get_console_by_role(role=role):
+        print("[green]{0} Success! User: {1}".format(get_current_function(), console_users))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('get_console_user_by_id')
+def get_console_user_by_id(user_id: int = typer.Argument(1)):
+    if console_user := ConsoleUserService().get_console_user_by_id(user_id=user_id):
+        print("[green]{0} Success! User: {1}".format(get_current_function(), dict(console_user)))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('update_console_user')
+def update_console_user(
+    user_id: int = typer.Argument(1),
+    console_user: str = typer.Argument('{"user_name": "test_user"}')
+):
+    if console_user := ConsoleUserService().update_console_user(
+        user_id=user_id,
+        console_user=User(**json.loads(console_user))
+    ):
+        print("[green]{0} Success! User: {1}".format(get_current_function(), dict(console_user)))
+    else:
+        print("[red]{0} Failed!".format(get_current_function()))
+
+
+@app.command('delete_console_user')
+def delete_console_user(user_id: int = typer.Argument(1)):
+    if is_success := ConsoleUserService().delete_console_user(user_id=user_id):
+        print("[green]{0} Success! User ID: {1}".format(get_current_function(), user_id))
     else:
         print("[red]{0} Failed!".format(get_current_function()))
 
