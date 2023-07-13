0765655b8c1e216d9b93a538419afb7b34938b83
윤범진 <unknown>
2023-07-13 15:19:06 +0900

    [UPDATE]디폴트 console user 로직 추가

--- server/database.py
+++ server/database.py
@@ -1,7 +1,9 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
+
+from server.auth.models import ConsoleUser, Role
 
 
 sqlite_file_name = "arbiter_test.db"
@@ -19,9 +21,12 @@ async_engine = create_async_engine(
 )
 
 
+# TODO: 마이그레이션 로직 추가되면 제거
 async def create_db_and_tables():
     async with async_engine.begin() as conn:
+        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
+        await set_default_console_user()
 
 
 def make_async_session():
@@ -34,3 +39,20 @@ def make_async_session():
 async def get_async_session() -> AsyncSession:
     async with make_async_session() as session:
         yield session
+
+
+# TODO: 마이그레이션 로직 추가되면 마이그레이션 로직으로 이동
+async def set_default_console_user():
+    async with make_async_session() as session:
+        email = "initial@default.com"
+        state = select(ConsoleUser).where(ConsoleUser.email == email)
+        result = await session.exec(state)
+        if result.first() is None:
+            session.add(
+                ConsoleUser(
+                    email=email,
+                    password="password",
+                    role=Role.OWNER
+                )
+            )
+            await session.commit()
