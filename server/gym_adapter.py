17704a2c74ec9e4cb03225f90301a3162f4a3c4d
myeonggilju <mgju@kc-softmax.com>
2023-07-14 14:03:50 +0900

    [UPDATE] gym adapter hint and comprehension

--- server/gym_adapter.py
+++ server/gym_adapter.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Dict
 import asyncio
 import timeit
 import gymnasium as gym
@@ -7,30 +7,34 @@ from collections import deque
 
 class GymAdapter:
     def __init__(self, env: gym.Env) -> None:
-        self.env = env
-        self.message = asyncio.Queue()
-        self.actions = deque()
+        self.env: gym.Env = env
+        self.message: asyncio.Queue = asyncio.Queue()
+        self.actions: deque = deque()
     
-    def add_client_action(self, action: Any) -> None:
-        self.actions.append(action)
+    def add_client_action(self, agent_id: int | str, action: int) -> None:
+        self.actions.append((agent_id, action))
     
     async def run(self) -> None:
-        waiting_time, loop_per_sec = 0.1, 0.1
-        turn_start_time = timeit.default_timer()
-        terminated = False
+        waiting_time: float = 0.1
+        loop_per_sec: float = 0.1
+        turn_start_time: float = timeit.default_timer()
+        terminated: bool = False
         # multiagent일 경우에 dict으로 처리해야한다
         # 모든 유저가 죽었을 경우에도 terminated가 True가 되어 종료된다.
         while not terminated:
             # 약 1초에 10번 처리한다.
             await asyncio.sleep(waiting_time)
-            turn_start_time = timeit.default_timer()
-            action = {}
-            for data in self.actions: action.update(data)
+            turn_start_time: float = timeit.default_timer()
+            actions: Dict[str | int, int] = {
+                agent_id: action
+                for agent_id, action in self.actions
+            }
             # env의 step에서 action에 대한 타입 체크를 해야한다.
             # multiagent일 경우 step에서 dict으로 처리해야한다.
-            obs, rewards, terminated, truncateds, infos = self.env.step(action)
+            obs, rewards, terminated, truncateds, infos = self.env.step(actions)
             # TODO: multiagent의 경우를 생각해야한다.
-            if type(terminated) == dict: terminated = all(terminated.values())
+            if type(terminated) == dict:
+                terminated = all(terminated.values())
             # step의 결과인 update된 state를 queue에 넣는다. 
             await self.message.put(obs)
             elapsed_time = timeit.default_timer() - turn_start_time            
