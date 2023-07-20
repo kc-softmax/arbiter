995cbb785c37dd4aa5741d44676d878811b78133
jaehong0721 <newbie302@gmail.com>
2023-07-20 12:17:19 +0900

    [UPDATE] 채팅 서비스 및 소켓 통신 업데이트

--- server/auth/utils.py
+++ server/auth/utils.py
@@ -1,9 +1,13 @@
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from pydantic import ValidationError
 
+from server.config import settings
 from server.auth.models import LoginType
 from server.auth.constants import TOKEN_GENERATE_ALGORITHM
+from server.auth.schemas import TokenDataSchema
+from server.auth.exceptions import InvalidToken
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -23,3 +27,29 @@ def create_token(subject: str, login_type: LoginType,  key: str, expires_delta: 
                              key,
                              algorithm=TOKEN_GENERATE_ALGORITHM.HS256)
     return encoded_jwt
+
+
+# Temp
+# KMT-106과 병합될 때 제거
+def verify_token(token: str, is_refresh_token: bool = False):
+    token_data = decode_token(token, is_refresh_token)
+    if datetime.fromtimestamp(token_data.exp) < datetime.now():
+        raise InvalidToken
+    return token_data
+
+
+def decode_token(token: str, is_refresh_token: bool = False):
+    key = settings.JWT_REFRESH_SECRET_KEY if is_refresh_token else settings.JWT_ACCESS_SECRET_KEY
+    try:
+        payload = jwt.decode(
+            token, key, algorithms=[
+                TOKEN_GENERATE_ALGORITHM.HS256])
+
+        token_data = TokenDataSchema(
+            sub=payload.get("sub"),
+            exp=payload.get("exp"),
+            login_type="guest"
+        )
+    except (JWTError, ValidationError):
+        raise InvalidToken
+    return token_data
