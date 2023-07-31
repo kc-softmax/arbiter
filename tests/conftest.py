906ea8c102d81d455d5b84b08e11a1d504e68119
윤범진 <>
2023-07-31 12:43:52 +0900

    [CHORE]정리

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
 import asyncio
-from typing import Generator
-
 import pytest
 import pytest_asyncio
+from typing import Generator
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -10,34 +9,25 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from server.database import async_engine
 
 
+# event_loop fixture를 테스트 케이스마다 주입해줘야 한다. -> @pytest.mark.asyncio가 처리 해줌
 @pytest.fixture(scope="session")
-def event_loop(request) -> Generator:  # noqa: indirect usage
+def event_loop() -> Generator:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
-# request 테스트를 위한 fixture
 
-
-# @pytest_asyncio.fixture
-# async def async_client():
-#     async with AsyncClient(
-#             app=app,
-#             base_url=f"http://{settings.api_v1_prefix}"
-#     ) as client:
-#         yield client
-
-
+# 테스트 class마다 session을 생성하고, 테스트가 끝나면 session을 종료한다.
 @pytest_asyncio.fixture(scope="class")
 async def async_session() -> AsyncSession:
-    session = sessionmaker(
+    test_session = sessionmaker(
         async_engine, class_=AsyncSession, expire_on_commit=False
     )
 
-    async with session() as s:
+    async with test_session() as session:
         async with async_engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
-        yield s
+        yield session
 
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.drop_all)
