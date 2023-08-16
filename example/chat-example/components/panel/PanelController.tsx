6cf1fdfc78081b81c2c076944f3d7236fde312fb
Changsik Yoon <dbs2636@gmail.com>
2023-08-16 12:25:27 +0900

    [ADD] ChatInfo 추가

--- example/chat-example/components/panel/PanelController.tsx
+++ example/chat-example/components/panel/PanelController.tsx
@@ -3,20 +3,30 @@
 import { usePanel } from "@/app/hooks/usePanel";
 import StartChatPanel from "./StartChatPanel";
 import ChattingPanel from "./ChattingPanel";
+import { useState } from "react";
+import { ChatInfo } from "@/@types/Chat";
 
 const PanelController = () => {
   const { PanelStep, setActiveStep } = usePanel([
     "startChat",
     "chatting",
   ] as const);
+  const [chatInfo, setChatInfo] = useState<ChatInfo>({
+    name: "",
+  });
+
+  const startChat = (name: string) => {
+    setChatInfo({ name });
+    setActiveStep("chatting");
+  };
 
   return (
     <div>
       <PanelStep name="startChat">
-        <StartChatPanel onClick={() => setActiveStep("chatting")} />
+        <StartChatPanel next={startChat} />
       </PanelStep>
       <PanelStep name="chatting">
-        <ChattingPanel />
+        <ChattingPanel chatInfo={chatInfo} />
       </PanelStep>
     </div>
   );
--- example/chat-example/components/panel/StartChatPanel.tsx
+++ example/chat-example/components/panel/StartChatPanel.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+"use client";
 
-interface StartChatPanelProps extends React.HTMLAttributes<HTMLButtonElement> {}
+import React, { useState } from "react";
 
-const StartChatPanel = ({ onClick, ...props }: StartChatPanelProps) => {
+interface StartChatPanelProps {
+  next: (name: string) => void;
+}
+
+const StartChatPanel = ({ next }: StartChatPanelProps) => {
+  const [name, setName] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name) return alert("Please enter your name");
+
+    next(name);
+  };
+
   return (
     <section>
-      <div className="h-screen flex justify-center items-center">
-        <button {...props} className="btn btn-primary btn-lg" onClick={onClick}>
-          Start Chat
-        </button>
+      <div>
+        <form
+          className="h-screen flex flex-col gap-4 justify-center items-center"
+          onSubmit={onSubmit}
+        >
+          <input
+            type="text"
+            className="input input-bordered input-lg"
+            placeholder="Type Your Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button type="submit" className="btn btn-primary btn-lg">
+            Start Chat
+          </button>
+        </form>
       </div>
     </section>
   );
