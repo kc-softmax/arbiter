dcf94873c1ec9a51aaec3f17a68ecdced9f7208b
Changsik Yoon <dbs2636@gmail.com>
2023-08-18 17:42:55 +0900

    [ADD] ChatTabs 추가

--- example/chat-example/components/panel/ChattingPanel.tsx
+++ example/chat-example/components/panel/ChattingPanel.tsx
@@ -1,9 +1,11 @@
 import { ChatInfo } from "@/@types/chat";
 import { useChat } from "@/app/hooks/useChat";
+import { ChatTabType } from "@/const/actions";
 import { scrollToBottom } from "@/lib/dom-utils";
 import { useEffect, useRef } from "react";
 import ChatInputForm from "../chat/ChatInputForm";
 import ChatRoom from "../chat/ChatRoom";
+import ChatTabs from "../chat/ChatTabs";
 
 interface ChattingPanelProps {
   chatInfo: ChatInfo;
@@ -11,8 +13,14 @@ interface ChattingPanelProps {
 
 const ChattingPanel = ({ chatInfo }: ChattingPanelProps) => {
   const { id, token } = chatInfo;
-  const { roomId, messages, users, sendMessage, eventMessage } = useChat(token);
+  const { roomId, messages, users, sendMessage, eventMessage, changeRoom } =
+    useChat(token);
   const chatPanelRef = useRef<HTMLDivElement>(null);
+
+  const onChangeTabs = (tab: ChatTabType) => {
+    console.log("tab :>> ", tab);
+    changeRoom(tab);
+  };
 
   const sendChat = (message: string) => {
     console.log(id, message);
@@ -27,6 +35,7 @@ const ChattingPanel = ({ chatInfo }: ChattingPanelProps) => {
     <section>
       <div className="p-4 h-screen">
         <div className="flex flex-col gap-4 justify-center items-center h-full rounded-lg border-2 max-w-4xl mx-auto p-4">
+          <ChatTabs onChange={onChangeTabs} />
           <div ref={chatPanelRef} className="flex-1 w-full overflow-scroll">
             <ChatRoom
               chatInfo={chatInfo}
--- example/chat-example/const/actions.ts
+++ example/chat-example/const/actions.ts
@@ -5,6 +5,14 @@ export const ChatActions = {
   ERROR: "error",
   MESSAGE: "message",
   CONTROL: "control",
+  ROOM_CHANGE: "room_change",
 } as const;
 
 export type ChatActionType = (typeof ChatActions)[keyof typeof ChatActions];
+
+export const ChatTabList = {
+  ALL: "all",
+  PARTY: "party",
+} as const;
+
+export type ChatTabType = (typeof ChatTabList)[keyof typeof ChatTabList];
