f3cfd7b1200aec3cf603d4d9114e2297942f5840
Changsik Yoon <dbs2636@gmail.com>
2023-08-22 14:31:45 +0900

    [REFACTOR] 베이스 함수 추가 및 코드 간소화

--- example/chat-example/components/chat/ChatList.tsx
+++ example/chat-example/components/chat/ChatList.tsx
@@ -13,18 +13,16 @@ const ChatList = ({ messages, eventMessage }: ChatListProps) => {
     <ul className="flex flex-col gap-2 flex-1">
       {messages.map(({ message, time, user }, index) => (
         <li key={`${user}-${index}`}>
-          <ChatBubble userInfo={user} message={message} time={time} />
+          <ChatBubble user={user} message={message} time={time} />
         </li>
       ))}
 
       <li className="sticky bottom-0">
-        {eventMessage?.action === ChatActions.USER_JOIN ? (
-          <ChatNotification username={eventMessage.data.user.name} enter />
-        ) : null}
-        {eventMessage?.action === ChatActions.USER_LEAVE ? (
+        {eventMessage?.action === ChatActions.USER_JOIN ||
+        eventMessage?.action === ChatActions.USER_LEAVE ? (
           <ChatNotification
             username={eventMessage.data.user.name}
-            enter={!(eventMessage.action === "user_leave")}
+            enter={eventMessage.action === ChatActions.USER_JOIN}
           />
         ) : null}
       </li>
