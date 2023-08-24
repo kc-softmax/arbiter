3928b1d5aea3a805c0d66474156fd0206387d535
Changsik Yoon <dbs2636@gmail.com>
2023-08-24 12:37:24 +0900

    [ADD] 로딩 패널 추가

--- example/chat-example/components/panel/PanelController.tsx
+++ example/chat-example/components/panel/PanelController.tsx
@@ -3,21 +3,30 @@
 import { usePanel } from "@/hooks/usePanel";
 import ChattingPanel from "./ChattingPanel";
 import StartChatPanel from "./StartChatPanel";
+import LoadingIndicator from "@/app/loading";
 
 const PanelController = () => {
   const { PanelStep, setActiveStep } = usePanel([
     "startChat",
+    "loading",
     "chatting",
   ] as const);
 
   const startChat = () => {
-    setActiveStep("chatting");
+    setActiveStep("loading");
+
+    setTimeout(() => {
+      setActiveStep("chatting");
+    }, 1000);
   };
 
   return (
     <div>
       <PanelStep name="startChat">
         <StartChatPanel next={startChat} />
+      </PanelStep>
+      <PanelStep name="loading">
+        <LoadingIndicator />
       </PanelStep>
       <PanelStep name="chatting">
         <ChattingPanel />
