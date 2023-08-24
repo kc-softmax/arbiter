629d01968a25ecb0362e03baf7f1fe8eeb55c2ab
Changsik Yoon <dbs2636@gmail.com>
2023-08-24 12:37:24 +0900

    [ADD] 로딩 패널 추가

---
+++ example/chat-example/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingIndicator = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <div className="loading loading-spinner loading-lg" />
+    </section>
+  );
+};
+
+export default LoadingIndicator;
