6dfeacee608ef5ace9f21a172ee18a6710ebc8f8
Changsik Yoon <dbs2636@gmail.com>
2023-08-25 12:41:59 +0900

    [ADD] useLogin 추가

--- example/chat-example/components/panel/StartChatPanel.tsx
+++ example/chat-example/components/panel/StartChatPanel.tsx
@@ -1,68 +1,34 @@
 "use client";
 
-import { requestLogin, requestSignUp, updateUserInfo } from "@/api/auth";
+import { useLogin } from "@/hooks/useLogin";
 import { authAtom } from "@/store/authAtom";
 import { useSetAtom } from "jotai";
-import React, { useState } from "react";
+import { useState } from "react";
 
 interface StartChatPanelProps {
   next: () => void;
 }
 
 const StartChatPanel = ({ next }: StartChatPanelProps) => {
-  const [id, setID] = useState(
-    process.env.NODE_ENV === "development" ? "@admin.com" : ""
-  );
-  const [password, setPassword] = useState(
-    process.env.NODE_ENV === "development" ? "password" : ""
-  );
-  const [isSignUp, setIsSignUp] = useState(false);
-  const [statusMessage, setStatusMessage] = useState("");
   const setAuthInfo = useSetAtom(authAtom);
+  const [isSignUp, setIsSignUp] = useState(false);
 
-  const getIdFromToken = (token: string) => {
-    const payload = token.split(".")[1];
-    const { sub } = JSON.parse(atob(payload)) as {
-      sub: string;
-    };
-
-    return sub;
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!id || !password) return alert("Please enter your ID and password");
-
-    try {
-      if (isSignUp) {
-        setStatusMessage("Signing Up...");
-        await requestSignUp({ id, password });
-      }
-
-      setStatusMessage("Logging In...");
-      const tokens = await requestLogin({ id, password });
-
-      const { access_token } = tokens;
-
-      const idFromToken = getIdFromToken(access_token);
-
-      setAuthInfo({ id: idFromToken, token: access_token });
-
-      setStatusMessage("Updating User Info...");
-      await updateUserInfo({
-        id,
-        token: access_token,
-      });
-
-      setStatusMessage("Done!");
+  const { onSubmit, statusMessage, formControls } = useLogin({
+    dev: {
+      id: "@admin.com",
+      password: "password",
+    },
+    onSuccess: ({ id, token }) => {
+      setAuthInfo({ id, token });
       next();
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message);
-      }
-    }
-  };
+    },
+    onError: (error) => {
+      alert(error.message);
+    },
+    config: {
+      isSignUp,
+    },
+  });
 
   return (
     <section>
@@ -91,15 +57,13 @@ const StartChatPanel = ({ next }: StartChatPanelProps) => {
                 type="text"
                 className="input input-bordered input-lg join-item"
                 placeholder="Type Your Name"
-                onChange={(e) => setID(e.target.value)}
-                value={id}
+                {...formControls.id}
               />
               <input
                 type="password"
                 className="input input-bordered input-lg join-item"
                 placeholder="Type Your password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
+                {...formControls.password}
               />
             </div>
             <button type="submit" className="btn btn-primary btn-lg w-auto">
