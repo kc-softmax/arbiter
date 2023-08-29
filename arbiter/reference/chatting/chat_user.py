bbf681493457b842cebeefa33b7cf7a5160c2446
myeonggilju <mgju@kc-softmax.com>
2023-08-29 16:40:27 +0900

    [UPDATE] adapter attribute name and summary

--- arbiter/reference/chatting/chat_user.py
+++
@@ -1,9 +0,0 @@
-
-
-class ChatUser:
-    def __init__(self, username: str):
-        self.username: str = username
-        self.is_leave: bool = False
-    
-    def leave_room(self) -> None:
-        self.is_leave = True
