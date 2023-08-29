9fb2a8995af7bc081bc126e0bc30c2a0e4176078
jaehong0721 <newbie302@gmail.com>
2023-08-29 11:58:24 +0900

    [UPDATE] 메타 클래스 내 cls 변수 사용 제거

--- server/utils.py
+++ server/utils.py
@@ -4,13 +4,9 @@ from typing import Any
 
 class SchemaMeta(SQLModelMetaclass):
     def __new__(cls, name, bases, namespaces, **kwargs):
-        fields = cls.get_fields(cls, namespaces)
-
-        # 따로 추가한 omit 또는 pick 필드가 없으면 원래의 SQLModel의 메타 클래스로 생성
-        if fields == None:
-            return super().__new__(cls, name, bases, namespaces, **kwargs)
-        # 따로 추가한 필드로 셋팅
-        cls.set_new_fields(cls, bases, namespaces, fields)
+        fields, is_pick = cls.get_fields(cls, namespaces)
+        if fields:
+            cls.set_new_fields(cls, bases, namespaces, fields, is_pick)
         return super().__new__(cls, name, bases, namespaces, **kwargs)
 
     def get_fields(cls, namespaces: dict[str, Any]):
@@ -18,15 +14,12 @@ class SchemaMeta(SQLModelMetaclass):
         pick_fields = getattr(namespaces.get("Config", {}), "pick_fields", {})
 
         if not omit_fields and not pick_fields:
-            return None
+            return None, None
         if omit_fields and pick_fields:
             raise Exception("You should set only one fields(omit_fields or pick_fields)")
+        return pick_fields if pick_fields else omit_fields, True if pick_fields else False
 
-        cls.new_field_type = "omit" if omit_fields else "pick"
-
-        return omit_fields if omit_fields else pick_fields
-
-    def set_new_fields(cls, bases, namespaces: dict[str, Any], user_fields: Any | dict[Any, Any]):
+    def set_new_fields(cls, bases, namespaces: dict[str, Any], user_fields: Any | dict[Any, Any], is_pick: bool):
         fields = namespaces.get('__fields__', {})
         annotations = namespaces.get('__annotations__', {})
         for base in bases:
@@ -38,10 +31,8 @@ class SchemaMeta(SQLModelMetaclass):
         new_fields = {}
         new_annotations = {}
         for field in merged_keys:
-            new_fields_condition = (field not in user_fields) if cls.new_field_type == "omit" else (
-                field in user_fields
-            )
-            if not field.startswith('__') and new_fields_condition:
+            is_included = field in user_fields
+            if not field.startswith('__') and is_included if is_pick else not is_included:
                 new_annotations[field] = annotations.get(field, fields[field].type_)
                 new_fields[field] = fields[field]
         namespaces['__annotations__'] = new_annotations
