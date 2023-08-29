0339755471a36ab76dd1cb21cdc00b578bdce881
윤범진 <>
2023-08-29 11:30:03 +0900

    [CHORE] hint

--- server/chat/connection.py
+++ server/chat/connection.py
@@ -11,7 +11,7 @@ class ConnectionManager:
     def __init__(self) -> None:
         self.active_connections: dict[str, list[WebSocket]] = defaultdict(list)
 
-    async def connect(self, websocket: WebSocket, room_id: str, token: str) -> tuple[str, str]:
+    async def connect(self, websocket: WebSocket, room_id: str, token: str) -> str:
         # 유효하지 않은 토큰일지라도 그 에러를 응답으로 보내주기 위해서는 먼저 accept을 해줘야한다.
         await websocket.accept()
         token_data = verify_token(token)
