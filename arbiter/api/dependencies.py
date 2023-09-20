83e30eae7a379fe0a5f05b8c54e627e941d1b951
jaehong0721 <newbie302@gmail.com>
2023-09-20 18:38:27 +0900

    [UPDATE] session contextmanager 내 실행하도록 수정

--- arbiter/api/dependencies.py
+++ arbiter/api/dependencies.py
@@ -1,9 +1,13 @@
+import asyncio
 from fastapi import Depends
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
+from arbiter.api.auth.models import User
 
 from arbiter.api.database import make_async_session, unit_of_work
 
 
 async def get_uow(session: AsyncSession = Depends(make_async_session)):
-    async with unit_of_work.transaction(session) as uow:
-        yield uow
+    async with session:
+        async with unit_of_work.transaction(session) as uow:
+            yield uow
