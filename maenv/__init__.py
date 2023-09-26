edb54b4da3a2310e99bab6abf203b009f433694d
myeonggilju <mgju@kc-softmax.com>
2023-09-26 16:25:22 +0900

    [UPDATE] code summary

--- maenv/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.0.1"
-
-from .dusty.dusty_env import DustyEnv as DustyMultiEnv
-
-from .dusty.dusty_env import DustySingleEnv
--- maenv/dusty/game_objects/__init__.py
+++
@@ -1,4 +0,0 @@
-from .game_object import *
-from .energy import *
-from .bullet import *
-from .explosion import *
