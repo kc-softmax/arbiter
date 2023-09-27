f2f84433f5b4935504840e0c40efec7db3398831
myeonggilju <mgju@kc-softmax.com>
2023-09-27 15:47:23 +0900

    [UPDATE] adapter checkpoint to s3 and env engine

--- arbiter/api/live/engine.py
+++ arbiter/api/live/engine.py
@@ -13,11 +13,12 @@ from arbiter.api.live.data import LiveMessage
 from gymnasium.envs.registration import register, registry
 from ray.rllib.policy.policy import Policy
 from ray.rllib.algorithms.marwil.marwil_torch_policy import MARWILTorchPolicy
+from ray.train import Checkpoint
+
 
 class Adapter:
-    def __init__(self,
-        check_point: str = '/Users/jumyeonggil/workspace/github-kc-softmax/reinforcement-learning/checkpoint_010000'
-    ):
+    def __init__(self, path: str = 's3://arbiter-server/BC/checkpoint_010000/'):
+        check_point = Checkpoint(path)
         self.trainer: dict[str, MARWILTorchPolicy] = Policy.from_checkpoint(check_point)
 
     async def adapt(self, obs: np.ndarray):
@@ -80,48 +81,32 @@ class LiveAsyncEngine(LiveEngine):
         
     async def on(self, message: LiveMessage):
         # not override, change behavior
-        deserialize = {
-            message.src: json.loads(base64.urlsafe_b64decode(message.data))
-        }
-        self._listen_queue.put_nowait(deserialize)
+        if message.src in self.adapter_map:
+            adapted_message = await self.adapter_map[message.src].adapt(message)
+            self._emit_queue.put_nowait(adapted_message)
+        else:
+            self._emit_queue.put_nowait(message)
 
     async def pre_processing(self, turn_messages: dict[str, list[any]]):
-        for agent_id, adapter in self.adapter_map.items():
-            # env의 obs에 agent가 add되었는지 체크해야한다(유저가 들어왔을 때 run이 실행되고있는 타이밍이 안맞을 수 있다
-            # 동시에 시작하는 게임이라면 고려할 필요가 없다(env에 이미 유저가 존재한다)
-            if agent_id in self.env.obs:
-                turn_messages[agent_id] = await adapter.adapt(self.env.obs[agent_id])
+        NotImplementedError()
     
     async def post_processing(self, turn_messages: dict[str, list[any]]):
-        # run에서 다시 turn_messages를 할당하기 때문에 필요가 없지만, 나중에 무엇이 필요할지 생각해봐야겠다.
-        turn_messages.clear()
+        NotImplementedError()
     
     async def processing(self, turn_messages: dict[str, list[any]]):
-        # obs는 agent가 사용하고 infos를 client에 보낸다
-        self.obs, self.rewards, self.terminateds, self.truncateds, self.infos = self.env.step(turn_messages)
-        live_message = LiveMessage(
-            src='server',
-            target=None,
-            data=base64.urlsafe_b64encode(json.dumps(self.infos).encode()),
-            systemEvent=None
-        )
-        await self._emit_queue.put(live_message)
+        # await self._emit_queue.put(live_message)
+        NotImplementedError()
         
     async def emit(self):
         time_interval = 1 / self.frame_rate
         waiting_time = time_interval
-        # turn_messages = collections.deque()
+        turn_messages = collections.deque()
         while not self.terminate:
             waiting_time > 0 and await asyncio.sleep(waiting_time)
             turn_start_time = timeit.default_timer()
             current_message_count = self._listen_queue.qsize()
-            turn_messages = {
-                agent_id: [None]
-                for agent_id in self.env.agents
-            }
             for _ in range(current_message_count):
-                # turn_messages.appendleft(self._listen_queue.get_nowait())
-                turn_messages.update(self._listen_queue.get_nowait())
+                turn_messages.appendleft(self._listen_queue.get_nowait())
             try:
                 await self.pre_processing(turn_messages)
                 await self.processing(turn_messages)
@@ -165,3 +150,57 @@ class LiveAsyncEnvEngine(LiveAsyncEngine):
                 kwargs=kwargs.get('env_config', {})
             )
         return super().__new__(cls)
+    
+    async def on(self, message: LiveMessage):
+        # not override, change behavior
+        deserialize = {
+            message.src: json.loads(base64.urlsafe_b64decode(message.data))
+        }
+        self._listen_queue.put_nowait(deserialize)
+
+    async def pre_processing(self, turn_messages: dict[str, list[any]]):
+        for agent_id, adapter in self.adapter_map.items():
+            # env의 obs에 agent가 add되었는지 체크해야한다(유저가 들어왔을 때 run이 실행되고있는 타이밍이 안맞을 수 있다
+            # 동시에 시작하는 게임이라면 고려할 필요가 없다(env에 이미 유저가 존재한다)
+            if agent_id in self.env.obs:
+                turn_messages[agent_id] = await adapter.adapt(self.env.obs[agent_id])
+    
+    async def post_processing(self, turn_messages: dict[str, list[any]]):
+        # run에서 다시 turn_messages를 할당하기 때문에 필요가 없지만, 나중에 무엇이 필요할지 생각해봐야겠다.
+        turn_messages.clear()
+    
+    async def processing(self, turn_messages: dict[str, list[any]]):
+        # obs는 agent가 사용하고 infos를 client에 보낸다
+        self.obs, self.rewards, self.terminateds, self.truncateds, self.infos = self.env.step(turn_messages)
+        live_message = LiveMessage(
+            src='server',
+            target=None,
+            data=base64.urlsafe_b64encode(json.dumps(self.infos).encode()),
+            systemEvent=None
+        )
+        await self._emit_queue.put(live_message)
+        
+    async def emit(self):
+        time_interval = 1 / self.frame_rate
+        waiting_time = time_interval
+        # turn_messages = collections.deque()
+        while not self.terminate:
+            waiting_time > 0 and await asyncio.sleep(waiting_time)
+            turn_start_time = timeit.default_timer()
+            current_message_count = self._listen_queue.qsize()
+            turn_messages = {
+                agent_id: [None]
+                for agent_id in self.env.agents
+            }
+            for _ in range(current_message_count):
+                turn_messages.update(self._listen_queue.get_nowait())
+            try:
+                await self.pre_processing(turn_messages)
+                await self.processing(turn_messages)
+                await self.post_processing(turn_messages)
+            except Exception as e:
+                print(e)
+            elapsed_time = timeit.default_timer() - turn_start_time
+            waiting_time = time_interval - elapsed_time
+        print('emit task end')
+        await self._emit_queue.put_nowait(None)
