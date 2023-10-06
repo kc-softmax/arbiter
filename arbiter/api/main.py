6c4ef98beafc4a04a8e06d3cc2d17c95e92cf5db
n1sz <n1szkjh@gmail.com>
2023-10-06 15:43:50 +0900

    [UPDATE] 1차 수동 합치기

--- arbiter/api/main.py
+++ arbiter/api/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
+# app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
 app.include_router(auth_router)
 
 
