2b3149c4bba3cbb04089b3cb5f944816d81d10db
jaehong0721 <newbie302@gmail.com>
2023-10-23 16:57:42 +0900

    [UPDATE] UnitOfWork를 fast api router 외에서도 사용가능하도록 수정

--- arbiter/api/dependencies.py
+++ arbiter/api/dependencies.py
@@ -1,17 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from arbiter.api.database import make_async_session
 
 
-class UnitOfWork():
+class UnitOfWork:
+    # for fastapi depedns
     async def __call__(self, session: AsyncSession = Depends(make_async_session)):
-        async with session as async_session:
-            try:
-                yield session
-                await async_session.commit()
-            except Exception as e:
-                await async_session.rollback()
-                raise e
+        async with self.transaction(session) as session:
+            yield session
             
+    @asynccontextmanager
+    async def transaction(self, session: AsyncSession = make_async_session()):
+        try:
+            yield session
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            raise e
+        
+
 unit_of_work = UnitOfWork()
