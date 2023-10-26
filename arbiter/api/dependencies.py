a36defd0a9f5225e5faffb157446281b9b5d06b5
jaehong0721 <newbie302@gmail.com>
2023-10-26 15:58:55 +0900

    [FIX] database error

--- arbiter/api/dependencies.py
+++ arbiter/api/dependencies.py
@@ -8,17 +8,24 @@ from arbiter.api.database import make_async_session
 class UnitOfWork:
     # for fastapi depedns
     async def __call__(self, session: AsyncSession = Depends(make_async_session)):
-        async with self.transaction(session) as session:
-            yield session
-            
-    @asynccontextmanager
-    async def transaction(self, session: AsyncSession = make_async_session()):
         try:
             yield session
             await session.commit()
         except Exception as e:
+            print("########## __CALL__ ERROR ############", e)
             await session.rollback()
             raise e
         
+            
+    @asynccontextmanager
+    async def transaction(self):
+        async with make_async_session() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception as e:
+                print("########## TRANSACTION ERROR ############", e)
+                await session.rollback()
+                raise e
 
 unit_of_work = UnitOfWork()
