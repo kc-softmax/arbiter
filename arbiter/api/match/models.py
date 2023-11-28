aa0b356e080d10d355c54a5fe96d14dcc7d67d21
myeonggilju <mgju@kc-softmax.com>
2023-11-28 18:32:55 +0900

    [UPDATE] GameRooms model and manage user in game server engine

--- arbiter/api/match/models.py
+++ arbiter/api/match/models.py
@@ -4,28 +4,20 @@ from sqlmodel import Column, Field, String, Integer, Relationship
 from arbiter.api.models import PKModel, BaseSQLModel, TimestampModel
 
 
-class GameState(str, Enum):
-    AVAILABLE = "available"
-    FULL = "full"
-
-
 class UserState(str, Enum):
     JOIN = "join"
     LEFT = "left"
 
 
-# auth 도메인 모델들
 class GameRoomsBase(BaseSQLModel):
     game_id: str = Field(sa_column=Column(String(128), unique=True))
-    max_players: int | None = Field(sa_column=Column(Integer))
-    now_players: int | None = Field(sa_column=Integer, default=0)
-    game_state: GameState = Field(sa_column=String(25), default=GameState.AVAILABLE)
+    max_player: int | None = Field(sa_column=Column(Integer))
 
 
 class GameAccessBase(BaseSQLModel):
     # game_rooms와 관계
     user_id: str = Field(sa_column=Column(String(128)))
-    user_state: GameState = Field(sa_column=String(25), default=UserState.JOIN)
+    user_state: UserState = Field(sa_column=String(25), default=UserState.JOIN)
 
 
 class GameRooms(PKModel, GameRoomsBase, TimestampModel, table=True):
