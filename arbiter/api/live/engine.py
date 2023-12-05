61e995d80321461ac74efaae36044cba9fe44e56
myeonggilju <mgju@kc-softmax.com>
2023-12-05 11:56:52 +0900

    [UPDATE] live service with room and engine

--- arbiter/api/live/engine.py
+++ arbiter/api/live/engine.py
@@ -2,46 +2,29 @@ from __future__ import annotations
 import asyncio
 import collections
 import timeit
-# import gymnasium as gym
-import json
-import base64
-# import numpy as np
 
+from typing import AsyncIterator
 from asyncio.tasks import Task
 from contextlib import asynccontextmanager
 from arbiter.api.live.data import LiveMessage
-# from gymnasium.envs.registration import register, registry
-# from ray.rllib.policy.policy import Policy
-# from ray.rllib.algorithms.marwil.marwil_torch_policy import MARWILTorchPolicy
-# from ray.train import Checkpoint
 
 
 class Adapter:
-    pass
-    # def __init__(self, path: str = 's3://arbiter-server/BC/checkpoint_010000/'):
-    #     check_point = Checkpoint(path)
-    #     self.trainer: dict[str, MARWILTorchPolicy] = Policy.from_checkpoint(check_point)
+    async def adapt(self, obs: any):
+        NotImplementedError()
 
-    # async def adapt(self, obs: np.ndarray):
-    #     prediction: tuple = self.trainer['policy_1'].compute_single_action(obs)
-    #     action = prediction[0]
-    #     return [action]
 
 class LiveEngine:
 
     def __init__(self):
-        
         self.adapter_map: dict[str, Adapter] = collections.defaultdict()
-        
         self._emit_queue: asyncio.Queue = asyncio.Queue()
 
-    async def setup_user(self, user_id: str, user_name: str=None):
+    async def setup_user(self, user_id: str, user_name: str = None):
         pass
-        # raise NotImplementedError()
         
-    async def remove_user(self, user_id: str, user_name: str=None):
+    async def remove_user(self, user_id: str, user_name: str = None):
         pass
-        # raise  NotImplementedError()
         
     async def on(self, message: LiveMessage):
         # apply adapter ?
@@ -52,18 +35,16 @@ class LiveEngine:
             self._emit_queue.put_nowait(message)
 
     @asynccontextmanager
-    async def subscribe(self) -> LiveEngine:
+    async def subscribe(self) -> AsyncIterator[LiveEngine]:
         try:
             yield self
         finally:
             # finally check before release engine
-            
             pass
 
-    async def __aiter__(self):
+    async def __aiter__(self) -> AsyncIterator[LiveMessage]:
         try:
             while True:
-                #
                 yield await self.get()
         except Exception:
             pass
@@ -82,25 +63,23 @@ class LiveAsyncEngine(LiveEngine):
         self.terminate = False        
         self._listen_queue: asyncio.Queue = asyncio.Queue()
         self.emit_task: Task = asyncio.create_task(self.emit())
-        
+
     async def on(self, message: LiveMessage):
         self._listen_queue.put_nowait(message)
 
-    
-    async def processing(self, live_message: dict[str, list[any]]):
-        # await self._emit_queue.put(live_message)
+    async def processing(self, live_message: list[LiveMessage]):
         NotImplementedError()
         
     async def emit(self):
         time_interval = 1 / self.frame_rate
         waiting_time = time_interval
-        turn_messages = collections.deque()
+        turn_messages: collections.deque = collections.deque()
         while not self.terminate:
             waiting_time > 0 and await asyncio.sleep(waiting_time)
             turn_start_time = timeit.default_timer()
             current_message_count = self._listen_queue.qsize()
             for _ in range(current_message_count):
-                turn_messages.appendleft(self._listen_queue.get_nowait())
+                 turn_messages.appendleft(self._listen_queue.get_nowait())
             try:
                 await self.processing(turn_messages)
             except Exception as e:
