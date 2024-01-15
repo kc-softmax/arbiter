f0e74b7588cdf907774cdb316697220dd0ca02bc
jaehong0721 <newbie302@gmail.com>
2024-01-15 15:55:38 +0900

    [ADD] jsonb 타입 저장을 위한 베이스 클래스 추가

--- arbiter/api/models.py
+++ arbiter/api/models.py
@@ -1,7 +1,13 @@
-from sqlmodel import Field, SQLModel
+import copy
+from sqlalchemy import Column
+from sqlalchemy_json import mutable_json_type
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlmodel import Field, SQLModel, select
 from sqlmodel.main import SQLModelMetaclass
+from sqlmodel.ext.asyncio.session import AsyncSession
+
 from datetime import datetime
-from typing import Any
+from typing import Any, Type, TypeVar
 
 
 class SchemaMeta(SQLModelMetaclass):
@@ -61,3 +67,60 @@ class TimestampModel(BaseSQLModel):
     deprecated_at: datetime | None = Field(
         nullable=True
     )
+
+
+DT = TypeVar('DT', bound='BaseDocument')
+
+
+class BaseDocument(PKModel, TimestampModel):
+    data: dict = Field(sa_column=Column(mutable_json_type(dbtype=JSONB, nested=True)))
+
+    @classmethod
+    async def query_and(cls: Type[DT], session: AsyncSession, filters: dict[str, Any]) -> list[DT]:
+        query = select(cls)
+        for key, value in filters.items():
+            if isinstance(value, str):
+                query = query.where(cls.data[key].astext == value)
+            elif isinstance(value, int):
+                query = query.where(cls.data[key].as_integer() == value)
+            elif isinstance(value, bool):
+                query = query.where(cls.data[key].as_boolean() == value)
+            else:
+                query = query.where(cls.data[key] == value)
+        result = (await session.exec(query)).all()
+        # test
+        for document in result:
+            print("Queried!!!", document)
+        return result
+
+    @classmethod
+    async def get_by_id(cls: Type[DT], session: AsyncSession, id: int) -> DT | None:
+        return (await session.exec(select(cls).where(cls.id == id))).first()
+
+    @classmethod
+    async def add(cls: Type[DT], session: AsyncSession, data: DT) -> DT:
+        session.add(data)
+        await session.flush()
+        await session.refresh(data)
+        return data
+
+    @classmethod
+    async def update(cls: Type[DT], session: AsyncSession, id: int, **values) -> DT:
+        document = await cls.get_by_id(session, id)
+        assert document is not None
+
+        document.data = copy.deepcopy(document.data)
+        document.updated_at = datetime.utcnow()
+        for key, value in values.items():
+            document.data[key] = value
+
+        session.add(document)
+        await session.flush()
+        await session.refresh(document)
+        return document
+
+    @classmethod
+    async def delete(cls: Type[DT], session: AsyncSession, id: int) -> None:
+        document = await cls.get_by_id(id)
+        await session.delete(document)
+        await session.flush()
