fd80aedb8687d411af98c052bd5d6cb0aa25ca23
n1sz <n1szkjh@gmail.com>
2024-01-19 10:10:01 +0900

    [UPDATE] 커넥션, 서비스

--- arbiter/api/stream/data.py
+++ arbiter/api/stream/data.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 from arbiter.api.auth.models import GameUser
-from arbiter.api.stream.const import ArbiterSystemEvent
+from arbiter.api.stream.const import StreamSystemEvent
 
 
 @dataclass
 class StreamMessage:
     data: bytes
     game_user: GameUser
-    event_type: ArbiterSystemEvent = None
+    event_type: StreamSystemEvent = None
--- arbiter/service/absctract_service.py
+++ arbiter/service/absctract_service.py
@@ -1,19 +1,43 @@
 import uuid
 import asyncio
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from arbiter.api.stream.data import StreamMessage
+from arbiter.broker.base import MessageConsumerInterface, MessageProducerInterface
 
 
-class AbstractService(metaclass=ABCMeta):
+class AbstractService(ABC):
+
+    def __init__(
+        self,
+        producer: MessageProducerInterface,
+        consumer: MessageConsumerInterface,
+    ):
+        self.service_id = uuid.uuid4()
+        self.producer = producer
+        self.consumer = consumer
+        self.task: asyncio.Task = None
 
     @abstractmethod
-    def stop(self):
+    async def stop(self):
         pass
 
     @abstractmethod
-    def consume_message(self, message: StreamMessage):
+    async def consuming_message(self, message: StreamMessage):
         pass
 
     @abstractmethod
-    def produce_message(self, message: StreamMessage):
+    async def producing(self):
         pass
+
+    async def consume(self):
+        async for message in self.consumer.listen():
+            self.consuming_message(message)
+            print(f"message: {message}")
+
+    async def __aenter__(self):
+        # self.task = asyncio.create_task(self.consume())
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.stop()
+        await self.task
