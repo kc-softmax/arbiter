25014f9add4888db157174209d10114a92c4c841
jaehong0721 <newbie302@gmail.com>
2024-06-05 14:32:18 +0900

    UPDATE 서비스 시작 로직 변경

---
+++ newbiter/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# NOTE _new_service로 찾는 것은 루트 경로 내에서 서비스 파일을 찾기 위한 임시 조치
+def find_python_files_in_path(dir_path: str = './'):
+    service_file_name_suffix = '_new_service'
+    current_path = Path(dir_path)
+    python_files = [str(p).split('.py')[0] for p in current_path.iterdir() 
+                    if p.is_file() and p.suffix == '.py' and str(p).split('.py')[0].find(service_file_name_suffix) > 0]
+    return python_files
+
+def get_running_command(module_name:str, service_name:str):
+    # init에서 파라미터를 받으면 안됨..
+    # call에서 실행할 경우 run 없어도됨
+    return f'from {module_name} import {service_name}; {service_name}().launch();'
