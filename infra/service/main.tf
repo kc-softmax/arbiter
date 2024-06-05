b7175ffe19895258d6ac07c51e8c67823bc63ff4
myeonggilju <mgju@kc-softmax.com>
2024-06-05 14:41:09 +0900

    [UPDATE] subprocess manage by pid

--- infra/service/main.tf
+++ infra/service/main.tf
@@ -5,75 +5,75 @@ module "network" {
   vpc_cidr_block = var.cidr
 }
 
-module "sg" {
-  source = "./sg"
-
-  service_name = var.service_name
-  vpc_id       = module.network.vpc_id
-}
-
-module "lb" {
-  source = "./lb"
-
-  service_name      = var.service_name
-  vpc_id            = module.network.vpc_id
-  public_subnet1_id = module.network.public_subnet1_id
-  public_subnet2_id = module.network.public_subnet2_id
-  alb_sg_id         = module.sg.alb_sg_id
-  zone_name         = var.zone_name
-}
-
-module "compute" {
-  source = "./compute"
-
-  service_name      = var.service_name
-  ec2_sg_id         = module.sg.ec2_sg_id
-  public_subnet1_id = module.network.public_subnet1_id
-  public_subnet2_id = module.network.public_subnet2_id
-  cluster           = module.container.cluster
-  ec2_tg            = module.lb.ec2_tg
-  instance_type     = var.instance_type
-  key_pair          = var.key_pair
-}
-
-module "images" {
-  source       = "./images"
-  service_name = var.service_name
-}
-
-module "container" {
-  source = "./container"
-
-  service_name = var.service_name
-  ec2_tg       = module.lb.ec2_tg
-  alb          = module.lb.alb
-  image_url    = module.images.image_url
-  region       = var.region
-}
-
-module "cache" {
-  source = "./cache"
-
-  service_name       = var.service_name
-  redis_sg_id        = module.sg.redis_sg_id
-  private_subnet1_id = module.network.private_subnet1_id
-  private_subnet2_id = module.network.private_subnet2_id
-  cache_node_type  = var.cache_node_type
-}
-
-# module "rds" {
-#   source = "./rds"
+# module "sg" {
+#   source = "./sg"
 
 #   service_name = var.service_name
-#   example_public_subnet1_id = module.network.example_public_subnet1_id
-#   example_public_subnet2_id = module.network.example_public_subnet2_id
-#   example_rds_sg = module.sg.example_rds_sg
+#   vpc_id       = module.network.vpc_id
 # }
 
-module "domain" {
-  source = "./domain"
+# module "lb" {
+#   source = "./lb"
 
-  alb         = module.lb.alb
-  zone_name   = var.zone_name
-  record_name = var.record_name
-}
+#   service_name      = var.service_name
+#   vpc_id            = module.network.vpc_id
+#   public_subnet1_id = module.network.public_subnet1_id
+#   public_subnet2_id = module.network.public_subnet2_id
+#   alb_sg_id         = module.sg.alb_sg_id
+#   zone_name         = var.zone_name
+# }
+
+# module "compute" {
+#   source = "./compute"
+
+#   service_name      = var.service_name
+#   ec2_sg_id         = module.sg.ec2_sg_id
+#   public_subnet1_id = module.network.public_subnet1_id
+#   public_subnet2_id = module.network.public_subnet2_id
+#   cluster           = module.container.cluster
+#   ec2_tg            = module.lb.ec2_tg
+#   instance_type     = var.instance_type
+#   key_pair          = var.key_pair
+# }
+
+# module "images" {
+#   source       = "./images"
+#   service_name = var.service_name
+# }
+
+# module "container" {
+#   source = "./container"
+
+#   service_name = var.service_name
+#   ec2_tg       = module.lb.ec2_tg
+#   alb          = module.lb.alb
+#   image_url    = module.images.image_url
+#   region       = var.region
+# }
+
+# module "cache" {
+#   source = "./cache"
+
+#   service_name       = var.service_name
+#   redis_sg_id        = module.sg.redis_sg_id
+#   private_subnet1_id = module.network.private_subnet1_id
+#   private_subnet2_id = module.network.private_subnet2_id
+#   cache_node_type  = var.cache_node_type
+# }
+
+# # module "rds" {
+# #   source = "./rds"
+
+# #   service_name = var.service_name
+# #   example_public_subnet1_id = module.network.example_public_subnet1_id
+# #   example_public_subnet2_id = module.network.example_public_subnet2_id
+# #   example_rds_sg = module.sg.example_rds_sg
+# # }
+
+# module "domain" {
+#   source = "./domain"
+
+#   alb         = module.lb.alb
+#   zone_name   = var.zone_name
+#   record_name = var.record_name
+# }
--- infra/service/outputs.tf
+++ infra/service/outputs.tf
@@ -1,27 +1,27 @@
-output "cache" {
-  value = module.cache
-}
-output "compute" {
-  value = module.compute
-}
-output "images" {
-  value = module.images
-}
-output "container" {
-  value = module.container
-}
-output "domain" {
-  value = module.domain
-}
-output "lb" {
-  value = module.lb
-}
+# output "cache" {
+#   value = module.cache
+# }
+# output "compute" {
+#   value = module.compute
+# }
+# output "images" {
+#   value = module.images
+# }
+# output "container" {
+#   value = module.container
+# }
+# output "domain" {
+#   value = module.domain
+# }
+# output "lb" {
+#   value = module.lb
+# }
 output "network" {
   value = module.network
 }
 # output "rds" {
 #   value = module.rds
 # }
-output "sg" {
-  value = module.sg
-}
+# output "sg" {
+#   value = module.sg
+# }
