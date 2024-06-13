baec2b92e0d15e880721ea071150bb574d4e3365
myeonggilju <mgju@kc-softmax.com>
2024-06-13 10:19:35 +0900

    [UPDATE] move to util common function

--- arbiter/utils.py
+++ arbiter/utils.py
@@ -1,7 +1,8 @@
+import importlib
 from pathlib import Path
 
-# NOTE _new_service로 찾는 것은 루트 경로 내에서 서비스 파일을 찾기 위한 임시 조치
 
+# NOTE _new_service로 찾는 것은 루트 경로 내에서 서비스 파일을 찾기 위한 임시 조치
 
 def find_python_files_in_path(dir_path: str = './'):
     service_file_name_suffix = '_service'
@@ -9,6 +10,17 @@ def find_python_files_in_path(dir_path: str = './'):
     python_files = [str(p).split('.py')[0] for p in current_path.iterdir()
                     if p.is_file() and p.suffix == '.py' and str(p).split('.py')[0].find(service_file_name_suffix) > 0]
     return python_files
+
+
+def find_registered_services(python_files_in_root: list[str], abstract_service: type):
+    registered_services = []
+    # 서비스 파일(root아래)들을 import
+    for python_file in python_files_in_root:
+        importlib.import_module(python_file)
+    # import 되었으므로 AbstractService의 subclasses로 접근 가능
+    for service in abstract_service.__subclasses__():
+        registered_services.append(service)
+    return registered_services
 
 
 def get_running_command(module_name: str, service_name: str):
