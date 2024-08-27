7dd4f52cc11e45e41967dbfdd5676d7114b6af24
myeonggilju <mgju@kc-softmax.com>
2024-08-27 11:09:40 +0900

    [UPDATE] config parser and os environment

--- arbiter/api/app.py
+++ arbiter/api/app.py
@@ -1,4 +1,14 @@
+import json
+import os
+import uuid
+
 from arbiter.api import ArbiterApiApp
+from arbiter.runner.utils import create_config
+from arbiter.utils import (
+    get_arbiter_setting,
+    read_config,
+)
+from arbiter.constants import CONFIG_FILE
 
 from uvicorn.server import Server
 from uvicorn.config import Config
@@ -7,7 +17,24 @@ from uvicorn.config import Config
 class ArbiterUvicornWorker:
 
     @staticmethod
-    async def run(app: ArbiterApiApp, host: str, port: int, log_level: str = 'info'):
-        config = Config(app, host=host, port=port, log_level=log_level)
+    async def run(app: ArbiterApiApp, unique_id: str | None = None):
+        if unique_id is None:
+            unique_id = uuid.uuid4().hex
+
+        arbiter_setting, is_arbiter_setting = get_arbiter_setting(CONFIG_FILE)
+        if not is_arbiter_setting:
+            create_config(arbiter_setting)
+        config = read_config(arbiter_setting)
+
+        host = config.get("server", "host", fallback="localhost")
+        port = config.get("server", "port", fallback="8080")
+        log_level = config.get("server", "log_level", fallback="error")
+        broker_config = dict(config['broker'])
+        server_config = dict(config['server'])
+        os.environ["NODE_ID"] = unique_id
+        os.environ["BROKER_CONFIG"] = json.dumps(broker_config)
+        os.environ["SERVER_CONFIG"] = json.dumps(server_config)
+
+        config = Config(app=app, host=host, port=port, log_level=log_level)
         server = Server(config)
         await server.serve()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,12 @@
 from arbiter import ArbiterRunner, ArbiterApp
+from arbiter.api import get_app
+from arbiter.api.app import ArbiterUvicornWorker
 from tests.test_worker import TestWorker
 
-app = ArbiterApp()
-app.add_worker(TestWorker)
+# app = ArbiterApp()
+# # app.add_worker(TestWorker)
 
-if __name__ == '__main__':
-    ArbiterRunner.run(app)
+# if __name__ == '__main__':
+#     ArbiterRunner.run(app)
+
+ArbiterUvicornWorker.run(get_app())
