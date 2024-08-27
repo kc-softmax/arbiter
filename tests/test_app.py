809a4749a861014064e660e451eb1f72b8ff4e7f
myeonggilju <mgju@kc-softmax.com>
2024-08-27 11:27:40 +0900

    [UPDATE] test_app original

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,8 @@
 from arbiter import ArbiterRunner, ArbiterApp
-from arbiter.api import get_app
-from arbiter.api.app import ArbiterUvicornWorker
 from tests.test_worker import TestWorker
 
-# app = ArbiterApp()
-# # app.add_worker(TestWorker)
+app = ArbiterApp()
+app.add_worker(TestWorker)
 
-# if __name__ == '__main__':
-#     ArbiterRunner.run(app)
-
-ArbiterUvicornWorker.run(get_app())
+if __name__ == '__main__':
+    ArbiterRunner.run(app)
