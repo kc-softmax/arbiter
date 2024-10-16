15f15ebc704f97ef9ef16ddbbd9b0b2ef34aad85
n1sz <n1szkjh@gmail.com>
2024-10-16 12:23:21 +0900

    [UPDATE] reload arbiter

--- arbiter/runner/runner.py
+++ arbiter/runner/runner.py
@@ -1,26 +1,56 @@
+import importlib
+import os
+from pathlib import Path
 import signal
 import asyncio
+from typing import Any
 from rich.console import Console
+from watchfiles import awatch
 from arbiter.enums import (
     WarpInPhase,
     WarpInTaskResult,
 )
 from arbiter.runner.console import arbiter_console_context
-from arbiter.configs import ArbiterConfig,BrokerConfig
 from arbiter.node import ArbiterNode
 from arbiter.utils import wait_until
-from arbiter import Arbiter
 
 console = Console()
+
+def get_app(module: str, app: str) -> Any:
+    try:
+        module = importlib.import_module(module)
+    except ModuleNotFoundError as exc:
+        if exc.name != module:
+            raise exc from None
+        message = 'Could not import module "{module}".'
+        raise Exception(f"Could not import module {module}")
+
+    instance = module
+    try:
+        instance = getattr(instance, app)
+    except AttributeError:
+        raise Exception(f"app name {app} is not found in module {module}")
+
+    return instance
+
+def get_reload_dirs(module: str = None) -> list[Path]:
+    if not module:
+        reload_dirs = [Path(os.getcwd())]
+    else:
+        reload_dir = module.split(".")
+        reload_dir.pop()
+        reload_dir = f"{os.getcwd()}/" + ".".join(reload_dir)
+        reload_dirs = [Path(reload_dir)]
+    return reload_dirs
 
 class ArbiterRunner:
         
     @staticmethod
     def run(
-        app: ArbiterNode,
+        app: ArbiterNode | str,
         repl: bool = False,
+        reload: bool = False,
     ):        
-        
         async def arbiter_run(
             arbiter_app: ArbiterNode,
         ):
@@ -29,7 +59,8 @@ class ArbiterRunner:
             """
             def shutdown_signal_handler():
                 shutdown_event.set()
-            shutdown_event = asyncio.Event()
+                app.internal_shutdown_event.set()
+                
             loop = asyncio.get_running_loop()
             for sig in (signal.SIGINT, signal.SIGTERM):
                 loop.add_signal_handler(sig, shutdown_signal_handler)
@@ -113,18 +144,72 @@ class ArbiterRunner:
             except Exception as e:
                 # start_phase에서 발생한 예외를 처리한다.
                 console.print(f"[bold red]An error occurred when loading arbiter[/bold red] {e}")
+
+        async def watch_and_restart(app: ArbiterNode, reload_dirs: list[Path]):
+            task: asyncio.Task = None
+            try:
+                # 최초 실행
+                task = asyncio.create_task(arbiter_run(app))
+                async for changes in awatch(*reload_dirs, stop_event=app.internal_shutdown_event):
+                    """
+                        체인지가 아니라, app이 스스로 종료되었을 때 어떻게 알 수 있을까?
+                    """
+                    print(f"Detected changes: {changes}")
+                    shutdown_event.set()
+                    # 기존 작업 중단
+                    if task:
+                        try:
+                            await asyncio.wait_for(task, timeout=10)
+                        except asyncio.TimeoutError:
+                            print("Task timeout")
+                        except Exception as e:
+                            print(f"Task exception: {e}")
+                    # 재시작
+                    shutdown_event.clear()
+                    task = asyncio.create_task(arbiter_run(app))
+            except KeyboardInterrupt:
+                # Keyboard interrupt signal -> exit by reloading
+                pass
+            finally:
+                # Graceful shutdown
+                if task:
+                    shutdown_event.set()
+                    try:
+                        await asyncio.wait_for(task, timeout=10)
+                    except asyncio.TimeoutError:
+                        print("Final Task timeout")
+                    except Exception as e:
+                        print(f"Final Task exception: {e}")
+                    
+        if isinstance(app, str):
+            """
+            if app is string, it means that module path.
+                and then, import the module from the module path.
+            if reload is True, reload dir set below example.
+                a. module path is tests.app:app, reload dir is tests/
+                b. module path is app:app, reload dir is ./
+            """
+            module, _, app = app.partition(":")
+            assert module and app, "The module path must be in format <module>:<attribute>."
+            app = get_app(module, app)
+            reload_dirs = get_reload_dirs(module)
+        else:
+            reload_dirs = get_reload_dirs()
+                    
+        assert isinstance(app, ArbiterNode), f"instance must be ArbiterNode, but {module} is {type(app)}"
         
         ################## RUN #####################
-        """
-        Read the config file.
-        """
+        
+        shutdown_event = asyncio.Event()
+        
         try:
-            # Register signal handlers for graceful shutdown
-            asyncio.run(arbiter_run(app))
+            if not reload:
+                asyncio.run(arbiter_run(app))
+            else:
+                asyncio.run(watch_and_restart(app, reload_dirs))
         except SystemExit as e:
             console.print(f"SystemExit caught in main: {e.code}")
         except KeyboardInterrupt:
             console.print("KeyboardInterrupt caught in main")
         except Exception as e:
             console.print(f"Unhandled exception in main: {e}")
-        
